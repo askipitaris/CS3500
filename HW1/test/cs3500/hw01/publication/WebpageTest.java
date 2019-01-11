acf299744abf5925b8b3f261943ce599f96ad27c
askipitaris <alexskipitaris@gmail.com>
2019-01-10 19:33:48 -0500

    added more tests

--- HW1/test/cs3500/hw01/publication/WebpageTest.java
+++ HW1/test/cs3500/hw01/publication/WebpageTest.java
@@ -3,6 +3,7 @@ package cs3500.hw01.publication;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 // Tests for citing a Webpage
 public class WebpageTest {
@@ -20,11 +21,31 @@ public class WebpageTest {
   }
 
   /**
+   * Tests incorrect APA formatting for webpage citations
+   */
+  @Test
+  public void testCiteApaWrong() {
+    assertNotEquals("This is formatted incorrectly!",
+        "Retrieved Jan 10, 2019, CS 3500: Object-Oriented Design. "
+        + "https://course.ccs.neu.edu/cs3500/.", this.courseSite.citeApa());
+  }
+
+  /**
    * Tests MLA formatting for webpage citations
    */
   @Test
   public void testCiteMla() {
     assertEquals("\\CS 3500: Object-Oriented Design.\\ Web. Jan 10, 2019 "
         + "<https://course.ccs.neu.edu/cs3500/>", this.courseSite.citeMla());
+  }
+
+  /**
+   * Tests incorrect MLA formatting for webpage citations
+   */
+  @Test
+  public void testCiteMlaWrong() {
+    assertNotEquals("This is formatted incorrectly!",
+        "Retrieved Jan 10, 2019, https://course.ccs.neu.edu/cs3500/. "
+            + "CS 3500: Object-Oriented Design.", this.courseSite.citeMla());
   }
 }
