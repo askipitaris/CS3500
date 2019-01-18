b2dc50606055053b3d69c83ad98de0c2861a32cc
askipitaris <alexskipitaris@gmail.com>
2019-01-17 20:34:54 -0500

    Spelling errors

--- HW1/test/cs3500/hw01/duration/AbstractDurationFormatTest.java
+++ HW1/test/cs3500/hw01/duration/AbstractDurationFormatTest.java
@@ -29,9 +29,12 @@ public abstract class AbstractDurationFormatTest {
   // and must *not* directly say "new CompactDuration(...)" or
   // "new HmsDuration(...)"
 
-  @Test(expected = IllegalArgumentException.class)
-  public void wrongOrder() {
-    hms(1, 0, 0).format("t%");
+  @Test
+  public void testBasic() {
+    assertEquals("4, 04, 5, 05, 9, 09",
+        hms(4, 5, 9).format("%h, %H, %m, %M, %s, %S"));
+    assertEquals("%, 3600",
+        sec(3600).format("%%, %t"));
   }
 
   @Test
@@ -55,7 +58,7 @@ public abstract class AbstractDurationFormatTest {
   }
 
   @Test
-  public void testTripplePercent() {
+  public void testTriplePercent() {
     assertEquals("%3600",
         hms(1, 0, 0).format("%%%t"));
   }
@@ -69,11 +72,19 @@ public abstract class AbstractDurationFormatTest {
   }
 
   @Test
-  public void testDoubleSpecificers() {
+  public void testDoubleSpecifiers() {
     assertEquals("240",
         hms(0, 4, 0).format("%t"));
     assertEquals("%t",
         hms(0, 4, 0).format("%%t"));
+  }
+
+  @Test
+  public void testConcat() {
+    assertEquals("%t",
+        hms(0, 4, 0).format("%" + "%t"));
+    assertEquals("%t",
+        hms(0, 4, 0).format("%" + "%" + "t"));
   }
 
   @Test(expected = IllegalArgumentException.class)
@@ -81,8 +92,22 @@ public abstract class AbstractDurationFormatTest {
     hms(0, 5, 1).format("%y");
     sec(500).format("%T seconds is %m minutes");
     hms(0, 5, 1).format("%d:%02d:%02d");
-    sec(180).format("%");
   }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void trailingSpecifiers() {
+    sec(180).format("%");
+    hms(1, 0, 0).format("t%");
+    sec(1000).format("%h:%M:%S%");
+  }
+
+  @Test
+  public void testBackslash() {
+    assertEquals("\\3600",
+        hms(1,0,0).format("\\%t"));
+  }
+
+
 
   /*
     Leave this section alone: It contains two abstract methods to
